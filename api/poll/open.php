65535f0c7e07ce56e598f9433cccdb651fb1f24c
Noah Bass <bassjnoah@gmail.com>
2015-02-13 19:23:57 -0500

    updated poll/open

--- api/poll/open.php
+++ api/poll/open.php
@@ -13,19 +13,27 @@ print 'Opening the poll' . "\n";
  * clean out old messages in google voice
  */
 $sms = $voice->getUnreadSMS();
+$count = 0;
+
 foreach($sms as $s) {
     // delete message
     $voice->deleteMessage($s->id);
+    $count++;
 }
 
 $sms = $voice->getReadSMS();
 foreach($sms as $s) {
     // delete message
     $voice->deleteMessage($s->id);
+    $count++;
 }
+
+print $count . ' messages deleted' . "\n";
 
 
 /**
  * clean out the old responses in the responses table
  */
 $pdo->deleteFrom('responses')->execute();
+
+print 'Responses cleaned' . "\n";
